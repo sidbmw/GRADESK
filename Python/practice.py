300bc097e5693bb7e336eff6adb80c6525826e4e
Siddharth Natamai <siddharthnatamai@gmail.com>
2018-11-03 00:52:37 -0400

    Just some learning

--- Python/practice.py
+++ Python/practice.py
@@ -1,3 +1,13 @@
 #Author: Siddharth Natamai
-#Date:October 30, 2018
-print ("Hello World")
+#Date: November 2, 2018
+
+#!/usr/bin/env python
+
+
+import PySimpleGUI as sg
+
+layout = [[sg.Text('GRADESK', size=(30, 1), justification='center', font=("Helvetica", 25))],
+          [sg.Input()],
+          [sg.OK()] ]
+
+sg.Window('Enter login credentials').Layout(layout).Read()
