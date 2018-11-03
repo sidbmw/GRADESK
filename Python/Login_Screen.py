f361262549cdb6ea9b0e4683fcf2e8d640726e97
Siddharth Natamai <siddharthnatamai@gmail.com>
2018-11-03 18:26:10 -0400

    Login Basics completed
    Username and password can be read and verified. Next step is to encrypt password and align everything.

--- Python/Login_Screen.py
+++ Python/Login_Screen.py
@@ -1,26 +1,24 @@
-# Author: sidbmw
+# Author: Siddharth Natamai
 # Date: November 3, 2018
 
-#!/usr/bin/env python
+# !/usr/bin/env python
 
 import PySimpleGUI as sg
 
 layout = [[sg.Text('GRADESK', size=(30, 1), justification='center', font=("Helvetica", 25))],
-          [sg.Text('Username')],
-          [sg.Input()],
+          [sg.Text('Username', justification='center', font=("Helvetica", 20))],
+          [sg.Input((), justification='center')],
           [sg.Text('Password')],
           [sg.Input()],
           [sg.Button('Read', bind_return_key=True), sg.Exit()]]
 
-
 window = sg.Window('Enter Login Credentials', default_element_size=(40, 1)).Layout(layout)
-
 
 while True:
     event, values = window.Read()
     if event is None or event == 'Exit':
         break
-    if values[0] == 'test':
+    if (values[0] == 'test') & (values[1] == 'test'):
         print('Login SUCCESSFUL')
     else:
         print('Login FAILED!!')
