6a7a9c21d50467fbe632c1562df509085a431463
Siddharth Natamai <siddharthnatamai@gmail.com>
2018-11-07 10:20:23 -0500

    Stat of Course Selection Screen

--- Python/Login_Screen.py
+++ Python/Login_Screen.py
@@ -5,11 +5,11 @@
 
 import PySimpleGUI as sg
 
-layout = [[sg.Text('GRADESK', size=(30, 1), justification='center', font=("Helvetica", 25))],
-          [sg.Text('Username', size=(30, 1), pad=((150, 5), 3), justification='center')],
-          [sg.Input(pad=((150, 5), 3))],
-          [sg.Text('Password', size=(30, 1), pad=((150, 5), 3), justification='center')],
-          [sg.Input(pad=((150, 5), 3))],
+layout = [[sg.Text('GRADESK', size=(30, 1), pad=((5, 5), 3), justification='center', font=("Helvetica", 25))],
+          [sg.Text('Username', size=(30, 1), pad=((154, 150), 3), justification='center', )],
+          [sg.Input(pad=((150, 150), 3))],
+          [sg.Text('Password', size=(30, 1), pad=((154, 150), 3), justification='center', )],
+          [sg.Input(pad=((150, 150), 3))],
           [sg.Button('Read', bind_return_key=True), sg.Exit()]]
 
 window = sg.Window('Enter Login Credentials', default_element_size=(40, 1)).Layout(layout)
