900d23535d273879a6349bf0ab7c3fc5e5c230dd
Haoyang Dong <mdong1@ocdsb.ca>
2018-11-09 11:16:50 -0500

    mark about half done

--- Python/GUI/mark_gui.py
+++ Python/GUI/mark_gui.py
@@ -1,13 +1,22 @@
 import PySimpleGUI as sg
 
+student_name = 'Mike Dong'
 mark = [[],
         []]
 
-column = []
+column = [[sg.Text('Expectation  ', text_color = 'black', justification = 'left'), sg.InputText(' ', size= (10, 1))],
+[sg.Text('Mark            ', text_color = 'black', justification = 'left'), sg.InputText(' ', size= (10, 1))],
+[sg.Text('                  ', justification = 'right'), sg.Checkbox(' ')],
+[sg.Text('_'  * 100, size=(23, 1))]]
 
 
-layout = [[sg.Text('Mark entry', size=(17, 1), font=("Helvetica", 25), justification = 'center')],
-[sg.Radio('Test    ', 'RADIO1', default=True, text_color = 'red'), sg.Radio('Assignment    ', 'RADIO1', text_color = 'blue'), sg.Radio('Presentation    ', 'RADIO1', text_color = 'green'), sg.Radio('Quiz    ', 'RADIO1', text_color = 'Yellow'), sg.Checkbox(' ')]]
+
+
+
+layout = [[sg.Text('Mark entry - ' + student_name, size=(21, 1), font=("Helvetica", 15), justification = 'center')],
+[sg.Radio('Test             ', 'RADIO1', default=True, text_color = 'red'), sg.Radio('Assignment   ', 'RADIO1', text_color = 'blue')],
+[sg.Radio('Presentation ', 'RADIO1', text_color = 'green'), sg.Radio('Quiz           ', 'RADIO1', text_color = 'Yellow'), sg.Checkbox(' ')],
+[sg.Column(column,scrollable=True, size=(225,150))]]
 
 
 event, values  = sg.Window('Mark ', auto_size_text=True, default_element_size=(40, 1)).Layout(layout).Read()
