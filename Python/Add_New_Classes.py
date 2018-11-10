b2ceeb65c1ef6c4c687e5adda668152a7be0f02f
Siddharth Natamai <siddharthnatamai@gmail.com>
2018-11-10 16:47:39 -0500

    Database connection sucessful

--- Python/Add_New_Classes.py
+++ Python/Add_New_Classes.py
@@ -17,7 +17,8 @@ layout = [[sg.Text('Add New Classes', size=(30, 2), justification='center', font
           [sg.Input((), size=(20, 2), pad=((215, 150), 10))],
           [sg.Text('Year', size=(50, 1), justification='center', font=("Helvetica", 15))],
           [sg.DropDown((2016, 2017, 2018, 2019), size=(18, 2), pad=((214, 150), 10))],
-          [sg.ReadButton('Course Code', key='add_new_courses_button', size=(20, 2), pad=((205, 150), 10))]
+          [sg.ReadButton('Course Code', key='add_new_courses_button', size=(20, 2), pad=((205, 150), 10),
+                         bind_return_key=True)]
           ]
 
 window = sg.Window('Add New Courses', default_element_size=(40, 2)).Layout(layout)
--- Python/Database_Connection_Test.py
+++ Python/Database_Connection_Test.py
@@ -4,6 +4,6 @@ from __future__ import print_function
 
 import cx_Oracle
 
-con = cx_Oracle.connect('EOM@//localhost:1521/xe')
+con = cx_Oracle.connect('EOM/EOM@127.0.0.1/xe')
 print (con.version)
 con.close()
