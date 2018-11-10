6cb247bfdf850ec33775472db7b7fc3a8f6363d6
Siddharth Natamai <siddharthnatamai@gmail.com>
2018-11-10 11:42:17 -0500

    WIP....

--- Python/Add_New_Classes.py
+++ Python/Add_New_Classes.py
@@ -1,21 +1,25 @@
 # Author: Siddharth Natamai
-# Date: November 9, 2018
+# Date: November 10, 2018
 
 # !/usr/bin/env python
 
 
 import PySimpleGUI as sg
 
-
-sg.SetOptions(element_padding=(0, 0))
+# sg.SetOptions(element_padding=(0, 0))
 
 sg.ChangeLookAndFeel('DarkBlue')
 
-layout = [[sg.Text('Add New Courses', size=(30, 1), justification='center', font=("Helvetica", 25))],
-          [sg.DropDown('1 2 3 4 5 6')],
-          [sg.ReadButton('Add course', key='add_new_courses_button')]
-]
+layout = [[sg.Text('Add New Classes', size=(30, 2), justification='center', font=("Helvetica", 25))],
+          [sg.Text('Course Code', size=(50, 1), justification='center', font=("Helvetica", 15))],
+          [sg.Input((), size=(20, 2), pad=((205, 150), 10))],
+          [sg.Text('Period Number', size=(50, 1), justification='center', font=("Helvetica", 15))],
+          [sg.Input((), size=(20, 2), pad=((205, 150), 10))],
+          [sg.Text('Year', size=(50, 1), justification='center', font=("Helvetica", 15))],
+          [sg.DropDown((2016, 2017, 2018, 2019), size=(20, 2), pad=((205, 150), 10))],
+          [sg.ReadButton('Course Code', key='add_new_courses_button', size=(20, 2), pad=((205, 150), 10))]
+          ]
 
-window = sg.Window('Add New Courses', default_element_size=(40, 1)).Layout(layout)
+window = sg.Window('Add New Courses', default_element_size=(40, 2)).Layout(layout)
 
 event, values = window.Read()
