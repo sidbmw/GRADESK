9f42975071fe1fe612e73877d21b886b9a42630d
Haoyang Dong <mdong1@ocdsb.ca>
2018-11-14 14:04:16 -0500

    yeah Nov 14

--- Python/GUI/mark_gui.py
+++ Python/GUI/mark_gui.py
@@ -1,6 +1,5 @@
 import PySimpleGUI as sg
 
-numberOf = 1
 
 student_name = 'Mike Dong'
 mark = [[],
@@ -10,11 +9,11 @@ column = []
 
 #column.remove([sg.RealtimeButton('Add')])
 
-    column.append([sg.Text('Expectation  ', text_color = 'black', justification = 'left'), sg.InputText(' ', size= (10, 1))],)
-    column.append([sg.Text('Mark            ', text_color = 'black', justification = 'left'), sg.InputText(' ', size= (10, 1))],)
-    column.append([sg.Text('                  ', justification = 'right'), sg.Checkbox(' ')],)
-    column.append([sg.Text('_'  * 100, size=(23, 1))],)
-    column.append([sg.Button('Add', size = (23,2), key = 'key_add')],)
+column.append([sg.Text('Expectation  ', text_color = 'black', justification = 'left'), sg.InputText(' ', size= (10, 1))],)
+column.append([sg.Text('Mark            ', text_color = 'black', justification = 'left'), sg.InputText(' ', size= (10, 1))],)
+column.append([sg.Text('                  ', justification = 'right'), sg.Checkbox(' ')],)
+column.append([sg.Text('_'  * 100, size=(23, 1))],)
+column.append([sg.Button('Add', size = (23,2), key = 'key_add')],)
 
 layout = [[sg.Text('Mark entry - ' + student_name, size=(21, 1), font=("Helvetica", 15), justification = 'center')],
 [sg.Radio('Test             ', 'RADIO1', default=True, text_color = 'red'), sg.Radio('Assignment   ', 'RADIO1', text_color = 'blue')],
@@ -24,12 +23,11 @@ layout = [[sg.Text('Mark entry - ' + student_name, size=(21, 1), font=("Helvetic
 
 window  = sg.Window('Mark ', auto_size_text=True, default_element_size=(40, 1)).Layout(layout)
 
-
 while (True):
     # This is the code that reads and updates your window
     event, values = window.Read()
-    if event == 'Add':
-        numberOf += 1    #values[0], is the Test radio, values[1] is the Assignment, and so on one at a time
+    if 'key_add':
+         #values[0], is the Test radio, values[1] is the Assignment, and so on one at a time
 
         del column[len(column)-1]
         column.append([sg.Text('Expectation  ', text_color = 'black', justification = 'left'), sg.InputText(' ', size= (10, 1))],)
@@ -37,6 +35,7 @@ while (True):
         column.append([sg.Text('                  ', justification = 'right'), sg.Checkbox(' ')],)
         column.append([sg.Text('_'  * 100, size=(23, 1))],)
         column.append([sg.Button('Add', size = (23,2), key = 'key_add')],)
+        window  = sg.Window('Mark ', auto_size_text=True, default_element_size=(40, 1)).Layout(layout)
 
     if event == 'Quit'  or values is None:
         break
