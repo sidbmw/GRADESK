11ae4a3c033a1c8c3c8b3286f7263d6bae0d5465
Haoyang Dong <mdong1@ocdsb.ca>
2018-11-18 13:03:26 -0500

    Big Change
    Stuff moved around, dynamic GUI elements removed(checkboxes, add button)
    
    experimenting with passing variables from one file to another

--- Python/GUI/mark_gui.py
+++ Python/GUI/mark_gui.py
@@ -1,24 +1,23 @@
 import PySimpleGUI as sg
-
+#import marking_first
 
 student_name = 'Mike Dong'
 mark = [[],
         []]
 
+numberOf = 2
+
 column = []
 
 #column.remove([sg.RealtimeButton('Add')])
 
-column.append([sg.Text('Expectation  ', text_color = 'black', justification = 'left'), sg.InputText(' ', size= (10, 1))],)
-column.append([sg.Text('Mark            ', text_color = 'black', justification = 'left'), sg.InputText(' ', size= (10, 1))],)
-column.append([sg.Text('                  ', justification = 'right'), sg.Checkbox(' ')],)
-column.append([sg.Text('_'  * 100, size=(23, 1))],)
-column.append([sg.Button('Add', size = (23,2), key = 'key_add')],)
+for x in range(numberOf):
+    column.append([sg.Text('Expectation  ', text_color = 'black', justification = 'left'), sg.InputText(' ', size= (10, 1), key = 'expectation_key'  )],)
+    column.append([sg.Text('Mark            ', text_color = 'black', justification = 'left'), sg.InputText(' ', size= (10, 1), key = 'mark_key')],)
+    column.append([sg.Text('_'  * 100, size=(23, 1))],)
 
 layout = [[sg.Text('Mark entry - ' + student_name, size=(21, 1), font=("Helvetica", 15), justification = 'center')],
-[sg.Radio('Test             ', 'RADIO1', default=True, text_color = 'red'), sg.Radio('Assignment   ', 'RADIO1', text_color = 'blue')],
-[sg.Radio('Presentation ', 'RADIO1', text_color = 'green'), sg.Radio('Quiz           ', 'RADIO1', text_color = 'Yellow'), sg.Checkbox(' ')],
-[sg.Column(column,scrollable=True, size=(225,150))],
+[sg.Column(column,scrollable=True, size=(225,100))],
 [sg.Button('Previous Student', key = 'key_prev_stud'), sg.Button('Save', key = 'key_save'), sg.Button('Next Student', key = 'key_next_stud')]]
 
 window  = sg.Window('Mark ', auto_size_text=True, default_element_size=(40, 1)).Layout(layout)
@@ -26,7 +25,7 @@ window  = sg.Window('Mark ', auto_size_text=True, default_element_size=(40, 1)).
 while (True):
     # This is the code that reads and updates your window
     event, values = window.Read()
-    if 'key_add':
+    if 'key_add' == True:
          #values[0], is the Test radio, values[1] is the Assignment, and so on one at a time
 
         del column[len(column)-1]
