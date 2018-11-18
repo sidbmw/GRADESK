97a5a38c2bc2709866d122d6680c577fa76dedaa
Siddharth Natamai <siddharthnatamai@gmail.com>
2018-11-18 12:20:16 -0500

    Update Database_Connection_Test.py

--- Python/Database_Connection_Test.py
+++ Python/Database_Connection_Test.py
@@ -1,16 +1,22 @@
+#https://stackoverflow.com/questions/43565189/get-column-value-by-name-rather-than-position-in-cx-oracle
 
 from __future__ import print_function
 
 import cx_Oracle
-import db_config
+import collections
+
+# import dbconfig
 
 con = cx_Oracle.connect('EOM/EOM@127.0.0.1/xe')
-cur = con.cursor(scrollable = True)
+cur = con.cursor(scrollable=True)
 
-cur.execute("select * from dept order by deptno")
+cur.execute("select * from EOM_CLASS WHERE CLASS = 'ICS4U-01' ")
 
-cur.scroll(2, mode = "absolute")  # go to second row
-print(cur.fetchone())
+for row in cur:
+    # print(row)
+   # print(row[0])
+   # print(row[1])
+    v_class = row[0]
+    print(v_class)
 
-cur.scroll(-1)                    # go back one row
-print(cur.fetchone())
+    print("record fetch completed")
