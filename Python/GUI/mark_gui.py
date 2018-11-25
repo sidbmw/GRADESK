9840936f1e701bd3e3c325109c14867f1aa07323
Haoyang Dong <mdong1@ocdsb.ca>
2018-11-24 20:03:10 -0500

    Update mark_gui.py

--- Python/GUI/mark_gui.py
+++ Python/GUI/mark_gui.py
@@ -8,6 +8,9 @@ student_name = 'Mike Dong'
 mark = [[],
         []]
 
+
+
+
 column = []
 
 #column.remove([sg.RealtimeButton('Add')])
@@ -15,7 +18,7 @@ sg.ChangeLookAndFeel('DarkBlue')
 
 for x in range(int(marking_first.numberOfMark)):
     column.append([sg.Text('Expectation  ', text_color = 'black', justification = 'left'), sg.InputText(' ', size= (10, 1))],)
-    column.append([sg.Text('Mark            ', text_color = 'black', justification = 'left'), sg.InputText(' ', size= (10, 1))],)
+    column.append([sg.Text('Mark            ', text_color = 'black', justification = 'left'), sg.InputText(mark[[0],[x]], size= (10, 1))],)
     column.append([sg.Text('_'  * 100, size=(23, 1))],)
 
 layout = [[sg.Text('Mark entry - ' + student_name, size=(21, 1), font=("Helvetica", 15), justification = 'center')],
@@ -27,15 +30,8 @@ window  = sg.Window('Mark ', auto_size_text=True, default_element_size=(40, 1)).
 while (True):
     # This is the code that reads and updates your window
     event, values = window.Read()
-    if 'key_add' == True:
-         #values[0], is the Test radio, values[1] is the Assignment, and so on one at a time
-        del column[len(column)-1]
-        column.append([sg.Text('Expectation  ', text_color = 'black', justification = 'left'), sg.InputText(' ', size= (10, 1))],)
-        column.append([sg.Text('Mark            ', text_color = 'black', justification = 'left'), sg.InputText(' ', size= (10, 1))],)
-        column.append([sg.Text('                  ', justification = 'right'), sg.Checkbox(' ')],)
-        column.append([sg.Text('_'  * 100, size=(23, 1))],)
-        column.append([sg.Button('Add', size = (23,2), key = 'key_add')],)
-        window  = sg.Window('Mark ', auto_size_text=True, default_element_size=(40, 1)).Layout(layout)
+
+
 
     if event == 'Quit'  or values is None:
         break
