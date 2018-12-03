8143f6378c84ee83352dd1296abb3a0b03db60d2
Siddharth Natamai <siddharthnatamai@gmail.com>
2018-12-02 22:49:00 -0500

    Updating database sucessful!

--- Python/test_runs.py
+++ Python/test_runs.py
@@ -1,19 +1,15 @@
+import cx_Oracle
 import PySimpleGUI as sg
+import pandas as pd
+import sys
 
-# sg.SetOptions(element_padding=(10, 2))
+con = cx_Oracle.connect('EOM/EOM@127.0.0.1/xe')
+cur = con.cursor(scrollable=True)
 
-sg.ChangeLookAndFeel('DarkBlue')
+sql = "UPDATE EOM_CLASS SET CLASS = 'TMJ4M-55' WHERE CLASS = 'ICS4U-08'"
 
-layout = [[sg.Text(' Add New Class', font=("Helvetica", 25))],
-          [sg.Text('Code Code', pad=(75, 2), font=("Helvetica", 15))],
-          [sg.Input((), size=(10, 2), pad=(50, 0))],
-          [sg.Text(' Period Number', font=("Helvetica", 15))],
-          [sg.Input((), size=(20, 2), pad=((20, 0), 10))],
-          [sg.Text('Year', font=("Helvetica", 15))],
-          [sg.DropDown((2016, 2017, 2018, 2019), size=(20, 2))],
-          [sg.ReadButton('Course Code', size=(20, 2), pad=(45, 0))]
-          ]
+cur.execute(sql)
 
-window = sg.Window('Add New Courses', default_element_size=(40, 2)).Layout(layout)
+con.commit()
 
-event, values = window.Read()
+print(cur.rowcount, "record(s) affected")
