0da3e5304671795f8595e2835dfa2aa81939eb7e
Siddharth Natamai <siddharthnatamai@gmail.com>
2018-12-07 11:06:31 -0500

    nothing

--- Python/test_runs.py
+++ Python/test_runs.py
@@ -1,53 +1,44 @@
+# !/usr/bin/env python
+import cx_Oracle
+import PySimpleGUI as sg
+import pandas as pd
 import sys
-if sys.version_info[0] >= 3:
-    import PySimpleGUI as sg
-else:
-    import PySimpleGUI27 as sg
-import random
-import string
 
-"""
-    Demo application to show how to draw rectangles and letters on a Graph Element
-    This demo mocks up a crossword puzzle board
-    It will place a letter where you click on the puzzle
-"""
+con = cx_Oracle.connect('EOM/EOM@127.0.0.1/xe')
 
 
-BOX_SIZE = 25
+def table_example():
+    sg.SetOptions(auto_size_buttons=True)
+    filename = sg.PopupGetFile('filename to open', no_window=True, file_types=(("CSV Files", "*.csv"),))
+    # --- populate table with file contents --- #
+    if filename == '':
+        sys.exit(69)
+    data = []
+    header_list = []
+    button = sg.PopupYesNo('Does this file have column names already?')
+    if filename is not None:
+        try:
+            df = pd.read_csv(filename, sep=',', engine='python',
+                             header=None)  # Header=None means you directly pass the columns names to the dataframe
+            data = df.values.tolist()  # read everything else into a list of rows
+            if button == 'Yes':  # Press if you named your columns in the csv
+                header_list = df.iloc[0].tolist()  # Uses the first row (which should be column names) as columns names
+                data = df[
+                       1:].values.tolist()  # Drops the first row in the table (otherwise the header names and the first row will be the same)
+            elif button == 'No':  # Press if you didn't name the columns in the csv
+                header_list = ['column' + str(x) for x in
+                               range(len(data[0]))]  # Creates columns names for each column ('column0', 'column1', etc)
+        except:
+            sg.PopupError('Error reading file')
+            sys.exit(69)
 
-layout = [
-            [sg.Text('Crossword Puzzle Using PySimpleGUI'), sg.Text('', key='_OUTPUT_')],
-            [sg.Graph((800,800), (0,450), (450,0), key='_GRAPH_', change_submits=True, drag_submits=False)],
-            [sg.Button('Show'), sg.Button('Exit')]
-         ]
+    layout = [[sg.Table(values=data, headings=header_list, display_row_numbers=True,
+                        auto_size_columns=False, num_rows=min(25, len(data)))]]
 
-window = sg.Window('Window Title', ).Layout(layout).Finalize()
+    window = sg.Window('Table', grab_anywhere=False)
+    event, values = window.Layout(layout).Read()
 
-g = window.FindElement('_GRAPH_')
+    sys.exit(69)
 
-for row in range(16):
-    for col in range(16):
-        if random.randint(0,100) > 10:
-            g.DrawRectangle((col * BOX_SIZE + 5, row * BOX_SIZE + 3), (col * BOX_SIZE + BOX_SIZE + 5, row * BOX_SIZE + BOX_SIZE + 3), line_color='black')
-        else:
-            g.DrawRectangle((col * BOX_SIZE + 5, row * BOX_SIZE + 3), (col * BOX_SIZE + BOX_SIZE + 5, row * BOX_SIZE + BOX_SIZE + 3), line_color='black', fill_color='black')
 
-        g.DrawText('{}'.format(row * 6 + col + 1), (col * BOX_SIZE + 10, row * BOX_SIZE + 8))
-
-while True:             # Event Loop
-    event, values = window.Read()
-    print(event, values)
-    if event is None or event == 'Exit':
-        break
-    mouse = values['_GRAPH_']
-
-    if event == '_GRAPH_':
-        if mouse == (None, None):
-            continue
-        box_x = mouse[0]//BOX_SIZE
-        box_y = mouse[1]//BOX_SIZE
-        letter_location = (box_x * BOX_SIZE + 18, box_y * BOX_SIZE + 17)
-        print(box_x, box_y)
-        g.DrawText('{}'.format(random.choice(string.ascii_uppercase)), letter_location, font='Courier 25')
-
-window.Close()
+table_example()
