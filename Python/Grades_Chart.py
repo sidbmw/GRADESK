499d0892ae2abf0b5e4135be0035f8d3faa36db5
Siddharth Natamai <siddharthnatamai@gmail.com>
2018-12-13 22:56:15 -0500

    Dropdown Menu WORKS!!!

--- Python/Grades_Chart.py
+++ Python/Grades_Chart.py
@@ -8,7 +8,7 @@ cur = con.cursor(scrollable=True)
 
 # WIP FOR LISTBOX
 cur.execute("SELECT FIRST_NAME FROM EOM_STUDENTS")
-names = cur.fetchall()
+names_fetch = cur.fetchall()
 
 # arr_name = []
 # for row in names_fetch:
@@ -16,12 +16,12 @@ names = cur.fetchall()
 #
 # print(arr_name)
 
-
-# print(names_fetch[0][1])
+new_names = [n[0] for n in names_fetch]
+print(new_names)
 
 layout = [[sg.Text('Listbox with search')],
           [sg.Input(do_not_clear=True, size=(20, 1), enable_events=True, key='_INPUT_')],
-          [sg.Listbox(names, size=(20, 4), enable_events=True, key='_LIST_')],
+          [sg.Listbox(new_names, size=(20, 4), enable_events=True, key='_LIST_')],
           [sg.Button('Chrome'), sg.Button('Exit')]]
 
 window = sg.Window('Window Title').Layout(layout)
@@ -33,10 +33,10 @@ while True:  # Event Loop
     print(event, values)
     if values['_INPUT_'] != '':
         search = values['_INPUT_']
-        new_values = [x for x in names if search in x]
+        new_values = [x for x in new_names if search in x]
         window.Element('_LIST_').Update(new_values)
     else:
-        window.Element('_LIST_').Update(names)
+        window.Element('_LIST_').Update(new_names)
     if event == '_LIST_' and len(values['_LIST_']):
         sg.Popup('Selected ', values['_LIST_'])
 
