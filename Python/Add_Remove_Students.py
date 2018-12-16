9a31c0bdd6d7f05afec40d5d9759cc63b0d4ae19
Siddharth Natamai <siddharthnatamai@gmail.com>
2018-12-15 20:05:02 -0500

    Update Add_Remove_Students.py

--- Python/Add_Remove_Students.py
+++ Python/Add_Remove_Students.py
@@ -20,38 +20,36 @@ for x in range(int(number_Of_Students) - 1):
     scrollable_column = scrollable_column + [[sg.InputText(), sg.InputText(), sg.Button(button_text=" X ")]]
     print(x)
 
-
 layout = [[sg.Stretch(), sg.Text('Add Students', font=("Helvetica", 25)), sg.Stretch()],
           [sg.Stretch(), sg.Text('Course code needs to be fetched into here')],
           [sg.Text("                              First Name"), sg.Text("                                                      Last Name")],
-          [sg.Column(scrollable_column, scrollable=True, size=(650, 500))],
+          [sg.Column(scrollable_column, scrollable=True, size=(650, 500), vertical_scroll_only=True)],
           # [sg.Input(do_not_clear=True, size=(20, 1), enable_events=True, key='_INPUT_')],
           # [sg.Listbox(fetched_course_codes, size=(20, 4), enable_events=True, key='_LIST_')],
 
-          [sg.Stretch(), sg.ReadButton('Add Course', key='add_new_courses_button', size=(20, 2),
+          [sg.Stretch(), sg.ReadButton('Add Students', key='key_add_students', size=(20, 2),
                                        bind_return_key=True), sg.Stretch(), ]
           ]
 
 window = sg.Window('Add New Courses', default_element_size=(40, 2)).Layout(layout)
 
-while 'add_new_courses_button':
+while 'key_add_students':
     event, values = window.Read()
     if event is None or event == 'Exit':
         break
-    # if values['_INPUT_'] != '':
-    #     search = values['_INPUT_']
-    #     new_values = [x for x in fetched_course_codes if search in x]
-    #     window.Element('_LIST_').Update(new_values)
-    # else:
-    #     window.Element('_LIST_').Update(fetched_course_codes)
-    # if event == '_LIST_' and len(values['_LIST_']):
-    #     sg.Popup('Selected ', values['_LIST_'])
-    if event == 'key_add_students':
-        reopen = True
-        for x in range(len(classes)):
-            if values[x]:
-                cur.execute("DELETE FROM EOM_CLASS WHERE CLASS = :stuff", stuff=str(classes[x] + '/' + year[x]))
+        # if values['_INPUT_'] != '':
+        #     search = values['_INPUT_']
+        #     new_values = [x for x in fetched_course_codes if search in x]
+        #     window.Element('_LIST_').Update(new_values)
+        # else:
+        #     window.Element('_LIST_').Update(fetched_course_codes)
+        # if event == '_LIST_' and len(values['_LIST_']):
+        #     sg.Popup('Selected ', values['_LIST_'])
 
-        con.commit()
-        break
+    for x in range(int(student_names)):
+        cur.execute("DELETE FROM EOM_CLASS WHERE CLASS = :stuff", stuff=str(student_names[x]))
+
+    con.commit()
+    break
+
 window.Close()
