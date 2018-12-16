a3ecd7be91e95de21b56fe416b5fa25ac5f1b097
Siddharth Natamai <siddharthnatamai@gmail.com>
2018-12-15 19:22:37 -0500

    Update Add_Remove_Students.py

--- Python/Add_Remove_Students.py
+++ Python/Add_Remove_Students.py
@@ -19,8 +19,7 @@ scrollable_column = [[sg.InputText(), sg.Button(button_text=" X ")]]
 
 for x in range(int(number_Of_Students) - 1):
     scrollable_column = scrollable_column + [[sg.InputText(), sg.Button(button_text=" X ")]]
-    print(number_Of_Students)
-
+    print(x)
 layout = [[sg.Stretch(), sg.Text('Add/Remove Students', font=("Helvetica", 25)), sg.Stretch()],
           [sg.Stretch(), sg.Text('Course code needs to be fetched into here')],
           [sg.Column(scrollable_column, scrollable=True, size=(350, 200))],
