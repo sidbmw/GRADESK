2ea67355a96784b72d7418eb0b98522888e41749
Haoyang Dong <mdong1@ocdsb.ca>
2018-12-21 09:28:48 -0500

    Dec 21 last day of class

--- Python/mark_gui.py
+++ Python/mark_gui.py
@@ -23,8 +23,8 @@ con = cx_Oracle.connect('system/earluser@127.0.0.1/xe')
 cur = con.cursor(scrollable=True)
 sql_rows = getRows()
 
-mark = [[],
-        []]
+mark = [[''],
+        ['']]
 column = []  # part of the layout
 
 sg.ChangeLookAndFeel('DarkBlue')
@@ -33,10 +33,13 @@ sg.ChangeLookAndFeel('DarkBlue')
 if marking_first.quit_option ==False:
     for x in range(int(marking_first.numberOfMark)):
         column.append(
-            [sg.Text('Expectation  ', text_color='white', justification='left'), sg.InputText('', size=(10, 1))], )
+            [sg.Text('Expectation  ', text_color='white', justification='left'),
+             sg.InputText(mark[0][x], size=(10, 1))],)
         column.append(
             [sg.Text('Mark            ', text_color='white', justification='left'), sg.InputText('', size=(10, 1))], )
         column.append([sg.Text('_' * 100, size=(23, 1))], )
+
+    mark = [[], []]
 
     for x in range(int(sql_rows)):
         open_variable = True
@@ -45,15 +48,14 @@ if marking_first.quit_option ==False:
         layout = [[sg.Text('Mark entry - ' + student_name, size=(25, 1),
                            font=("Helvetica", 15), justification='center')],
                   [sg.Column(column, scrollable=True, size=(225, 300), vertical_scroll_only=True)],
-                  [sg.Button('Save', key='key_save'),
-                   sg.Button('Next Student', key='key_next_stud')]]
+                  [sg.Button('Next Student', key='key_next_stud')]]
 
         window = sg.Window('Mark ', auto_size_text=True, default_element_size=(40, 1)).Layout(layout)
 
         while open_variable:
             event, values = window.Read()
             saved = False
-            if event == 'key_save':
+            if event == 'key_next_stud':
                 for x in range(int(marking_first.numberOfMark)):
                     tracker = int(x * 2)
                     if values[tracker + 0] != None:
@@ -66,7 +68,6 @@ if marking_first.quit_option ==False:
                         sg.Popup('incomplete input')
 
                 for y in range(int(marking_first.numberOfMark)):
-
                     cur.execute("""
                         insert into EOM_MARKS (STUDENT_ID, COLOUR, TASK, EXPECTATION, MARK, COMMENTS, ANOMALY, DELETED_FLAG)
                         values (:studentID, :color, :nameOfMark, :task_variable, :mark_variable, :null_variable, 
@@ -84,14 +85,12 @@ if marking_first.quit_option ==False:
 
                     con.commit()
                     saved = True
-            if event == 'key_next_stud':
-                mark = [[], []]
 
+                mark = [[], []]
                 break
 
             if values is None:
                 sys.exit()
 
         window.Close()  # Don't forget to close your window!
-
     sg.Popup('You have just finished marking ' + marking_first.nameOfMark + '!')
