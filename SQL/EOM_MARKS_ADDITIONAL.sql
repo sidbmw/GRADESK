8d0bc8014b71ba742f07cc0f029083986115464e
Siddharth Natamai <siddharthnatamai@gmail.com>
2018-12-22 14:01:49 -0500

    Update EOM_MARKS_ADDITIONAL.sql

--- SQL/EOM_MARKS_ADDITIONAL.sql
+++ SQL/EOM_MARKS_ADDITIONAL.sql
@@ -2,6 +2,19 @@
 --  File created - Saturday-December-22-2018   
 --------------------------------------------------------
 REM INSERTING into EOM.EOM_MARKS
+DROP TABLE eom_marks;
+CREATE TABLE eom_marks
+(
+  student_id   NUMBER       NOT NULL,
+  colour       VARCHAR2(20) NOT NULL,
+  task         VARCHAR(2)   NOT NULL,
+  expectation  VARCHAR2(2)  NOT NULL,
+  mark         VARCHAR2(5)  NOT NULL,
+  comments     VARCHAR2(250),
+  anomaly      VARCHAR2(1)  NOT NULL,
+  deleted_flag VARCHAR2(1)  NOT NULL
+);
+
 SET DEFINE OFF;
 Insert into EOM.EOM_MARKS (STUDENT_ID,COLOUR,TASK,EXPECTATION,MARK,COMMENTS,ANOMALY,DELETED_FLAG) values (1,'blue','T2','A3','3+',null,'N','N');
 Insert into EOM.EOM_MARKS (STUDENT_ID,COLOUR,TASK,EXPECTATION,MARK,COMMENTS,ANOMALY,DELETED_FLAG) values (1,'blue','T1','C3','3+/4-',null,'N','N');
