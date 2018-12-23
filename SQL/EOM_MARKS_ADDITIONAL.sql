8c064c35a4ed52be8a62994ab10e2762c85548a9
Siddharth Natamai <siddharthnatamai@gmail.com>
2018-12-23 09:03:18 -0500

    Update EOM_MARKS_ADDITIONAL.sql

--- SQL/EOM_MARKS_ADDITIONAL.sql
+++ SQL/EOM_MARKS_ADDITIONAL.sql
@@ -41,33 +41,33 @@ Insert into EOM.EOM_MARKS (STUDENT_ID,COLOUR,TASK,EXPECTATION,MARK,COMMENTS,ANOM
 
 drop table eom_main_screen_layout;
 
-CREATE TABLE eom_main_screen_layout
-(
-  student_id NUMBER NOT NULL,
-  x_INC      VARCHAR(2),
-  x_R        VARCHAR(2),
-  x_1mm      VARCHAR(2), -- 1--
-  x_1ms1     VARCHAR(2), --1-/1--
-  x_1        VARCHAR(2), --1--
-  x_1s1p     VARCHAR(2), --1/1+--
-  x_1p       VARCHAR(2), --1+--
-  x_1ps2m    VARCHAR(2), --1+/2--
-  x_2m       VARCHAR(2), 
-  x_2ms2     VARCHAR(2), 
-  x_2        VARCHAR(2), 
-  x_2s2p     VARCHAR(2),
-  x_2p       VARCHAR(2),
-  x_2ps3m    VARCHAR(2),
-  x_3m       VARCHAR(2),
-  x_3ms3     VARCHAR(2),
-  x_3        VARCHAR(2),
-  x3s3p      VARCHAR(2),
-  x_3p       VARCHAR(2),
-  x_3ps4m    VARCHAR(2),
-  x_4m       VARCHAR(2),
-  x_4ms4     VARCHAR(2),
-  x_4        VARCHAR(2),
-  x4s4p      VARCHAR(2),
-  x_4p       VARCHAR(2),
-  x_4pp      VARCHAR(2)
+CREATE TABLE eom_main_screen_layout (
+    student_id    NUMBER NOT NULL,
+    expectation   VARCHAR(2) NOT NULL,
+    x_inc         VARCHAR(2),
+    x_r           VARCHAR(2),
+    x_1mm         VARCHAR(2), -- 1--
+    x_1ms1        VARCHAR(2), --1-/1--
+    x_1           VARCHAR(2), --1--
+    x_1s1p        VARCHAR(2), --1/1+--
+    x_1p          VARCHAR(2), --1+--
+    x_1ps2m       VARCHAR(2), --1+/2--
+    x_2m          VARCHAR(2),
+    x_2ms2        VARCHAR(2),
+    x_2           VARCHAR(2),
+    x_2s2p        VARCHAR(2),
+    x_2p          VARCHAR(2),
+    x_2ps3m       VARCHAR(2),
+    x_3m          VARCHAR(2),
+    x_3ms3        VARCHAR(2),
+    x_3           VARCHAR(2),
+    x3s3p         VARCHAR(2),
+    x_3p          VARCHAR(2),
+    x_3ps4m       VARCHAR(2),
+    x_4m          VARCHAR(2),
+    x_4ms4        VARCHAR(2),
+    x_4           VARCHAR(2),
+    x4s4p         VARCHAR(2),
+    x_4p          VARCHAR(2),
+    x_4pp         VARCHAR(2)
 );
