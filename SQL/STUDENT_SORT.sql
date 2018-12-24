9f578f9014ac502a81f1fe58de51c693aa1ee1c2
Siddharth Natamai <siddharthnatamai@gmail.com>
2018-12-24 13:02:50 -0500

    Create STUDENT_SORT.sql

---
+++ SQL/STUDENT_SORT.sql
@@ -0,0 +1,31 @@
+DECLARE
+    v_sort_id   NUMBER:= 1;
+BEGIN
+
+    FOR r_std IN (
+        SELECT
+            class,
+            last_name,
+            first_name,
+            ROWID
+        FROM
+            eom_students
+        ORDER BY
+            class,
+            last_name,
+            first_name
+    ) LOOP
+        UPDATE eom_students
+        SET
+            sort_id = v_sort_id
+        WHERE
+            rowid = r_std.rowid;
+
+        v_sort_id := v_sort_id + 1;
+    END LOOP;
+
+END;
+/
+
+
+SELECT * FROM EOM_STUDENTS ORDER BY CLASS, LAST_NAME, FIRST_NAME;
