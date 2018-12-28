babccd6266b4d6107cc048744a731504f7f21f37
Siddharth Natamai <siddharthnatamai@gmail.com>
2018-12-28 11:55:18 -0500

    Dropdown menu completed

--- Python/Grades_Chart_Dropdown_Student_Selection.py
+++ Python/Grades_Chart_Dropdown_Student_Selection.py
@@ -4,29 +4,29 @@ import PySimpleGUI as sg
 con = cx_Oracle.connect('EOM/EOM@127.0.0.1/xe')
 cur = con.cursor(scrollable=True)
 
-cur.execute("SELECT LAST_NAME FROM EOM_STUDENTS")
-last_name = cur.fetchall()
-last_name = [n[0] for n in last_name]
-print(last_name)
+cur.execute("SELECT LAST_NAME || ', ' || FIRST_NAME FROM EOM_STUDENTS")
+full_name = cur.fetchall()
+full_name = [n[0] for n in full_name]
+# print(full_name)
 
-cur.execute("SELECT FIRST_NAME FROM EOM_STUDENTS")
-first_name = cur.fetchall()
-first_name = [n[0] for n in first_name]
-print(first_name)
+cur.execute("SELECT COUNT(*) FROM EOM_STUDENTS")
+num_of_rows = cur.fetchall()
+num_of_rows = [n[0] for n in num_of_rows]
+num_of_rows = num_of_rows[0]
+# print(num_of_rows)
 
-full_name = ''
-for i in range(0, len(first_name)):
-    full_name += last_name[i] + str(", ") + first_name[i] + str('    ')
+# i = 0
+# first_name = ''
+# while i < num_of_rows:
+#     first_name = first_name + str(full_name[i].split(", "))
+#     i += 1
+# print(first_name)
 
-print(full_name)
-
-# full_name = last_name + str(", ") + first_name
 
 layout = [[sg.Text('Listbox with search')],
           [sg.Input(do_not_clear=True, size=(20, 1), enable_events=True, key='_INPUT_')],
           [sg.Listbox(full_name, size=(60, 10), enable_events=True, key='_LIST_')],
-          [sg.Button('Chrome'), sg.Button('Exit')]]
-
+          [sg.Button('Exit')]]
 
 window = sg.Window('Window Title').Layout(layout)
 
@@ -34,7 +34,7 @@ while True:  # Event Loop
     event, values = window.Read()
     if event is None or event == 'Exit':
         break
-    print(event, values)
+    # print(event, values)
 
     if values['_INPUT_'] != '':
         search = values['_INPUT_']
@@ -45,5 +45,22 @@ while True:  # Event Loop
 
     if event == '_LIST_' and len(values['_LIST_']):
         sg.Popup('Selected ', values['_LIST_'])
+
+        # print(values['_LIST_'][0])
+        full_name_split = values['_LIST_'][0].split(", ")
+        last_name = full_name_split[0]
+        first_name = full_name_split[1]
+
+        # print(first_name)
+
+        cur.execute("SELECT STUDENT_ID, CLASS FROM EOM_STUDENTS WHERE FIRST_NAME = :first_name AND LAST_NAME = :last_name", first_name=first_name, last_name=last_name)
+        student_id_and_class = cur.fetchall()
+        student_id = [n[0] for n in student_id_and_class]
+        student_id = student_id[0]
+        class_code = [n[1] for n in student_id_and_class]
+        class_code = class_code[0]
+
+        # student id and class code fetched from selected name (in dropdown menu)
+        print(student_id, class_code)
 
 window.Close()
