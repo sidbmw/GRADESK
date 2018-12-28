0ff9f802e05e5b7f6146de182e9f257af730ae69
Siddharth Natamai <siddharthnatamai@gmail.com>
2018-12-28 00:29:39 -0500

    Update Grades_Chart.py

--- Python/Grades_Chart.py
+++ Python/Grades_Chart.py
@@ -17,7 +17,7 @@ min_sort_id = cur.fetchall()
 min_sort_id = [n[0] for n in min_sort_id]
 min_sort_id = min_sort_id[0]
 # sort_id = min_sort_id
-sort_id = 11
+sort_id = 1
 
 while event != 'close_window':
     sort_id = cur.execute("select sort_id from EOM_STUDENTS where SORT_ID = :sort_id", sort_id=sort_id)
