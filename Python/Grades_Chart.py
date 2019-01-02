a7847ff5eea79b3c7eb32f38c6d249b83f8b961b
Siddharth Natamai <siddharthnatamai@gmail.com>
2019-01-02 12:08:00 -0500

    Update Grades_Chart.py

--- Python/Grades_Chart.py
+++ Python/Grades_Chart.py
@@ -18,6 +18,7 @@ min_sort_id = [n[0] for n in min_sort_id]
 min_sort_id = min_sort_id[0]
 # sort_id = min_sort_id
 <<<<<<< HEAD
+<<<<<<< HEAD
 sort_id = None
 
 while event != 'close_window':
@@ -93,6 +94,79 @@ while event != 'close_window':
 
                 # print(raw_data)
 
+=======
+sort_id = 1
+
+while event != 'close_window':
+    sort_id = cur.execute("select sort_id from EOM_STUDENTS where SORT_ID = :sort_id", sort_id=sort_id)
+    sort_id = cur.fetchall()
+    sort_id = [n[0] for n in sort_id]
+    sort_id = sort_id[0]
+
+    student_id = cur.execute("SELECT STUDENT_ID FROM EOM_STUDENTS WHERE SORT_ID = :sort_id", sort_id=sort_id)
+    student_id = cur.fetchall()
+    student_id = [n[0] for n in student_id]
+    student_id = student_id[0]
+
+    student_first_name = cur.execute("SELECT FIRST_NAME FROM EOM_STUDENTS WHERE STUDENT_ID = :student_id", student_id=student_id)
+    student_first_name = cur.fetchall()
+    student_first_name = [n[0] for n in student_first_name]
+    student_first_name = student_first_name[0]
+
+    student_last_name = cur.execute("SELECT LAST_NAME FROM EOM_STUDENTS WHERE STUDENT_ID = :student_id", student_id=student_id)
+    student_last_name = cur.fetchall()
+    student_last_name = [n[0] for n in student_last_name]
+    student_last_name = student_last_name[0]
+
+    student_full_name = student_last_name + str(", ") + student_first_name
+    # print(student_first_name, student_last_name)
+
+    class_code = cur.execute("SELECT CLASS FROM EOM_STUDENTS WHERE STUDENT_ID = :student_id", student_id=student_id)
+    class_code = cur.fetchall()
+    class_code = [n[0] for n in class_code]
+    class_code = class_code[0]
+
+    cur.callproc('eom_build_layout', [student_id])
+
+    scrollable_column = [[sg.Graph((1800, 700), (0, 450), (450, -60), key='_GRAPH_', change_submits=True, drag_submits=False)]]
+
+    layout = [
+        [sg.Text(student_full_name, font='Helvetica', size=(40, 1)), sg.Text(class_code, font='Helvetica', size=(40, 1))],
+        [sg.Column(scrollable_column, scrollable=True, size=(1800, 700), vertical_scroll_only=True)],
+        [sg.Button('Previous Student', key='_prev_student_'), sg.Button('Next Student', key='_next_student_'), sg.Button("Exit", key="close_window")]
+    ]
+
+    window = sg.Window(student_full_name).Layout(layout).Finalize()
+
+    g = window.FindElement('_GRAPH_')
+
+    cur.execute("SELECT COUNT (*) FROM EOM_MAIN_SCREEN_LAYOUT WHERE STUDENT_ID = :student_id", student_id=student_id)
+    v_num_of_rows = cur.fetchall()
+    v_num_of_rows = [n[0] for n in v_num_of_rows]
+
+    # print(v_num_of_rows[0])
+
+    for row in range(v_num_of_rows[0]):
+        for col in range(27):
+
+            if row == 0:
+                arr_marks = ['Expectation', 'INC', 'R', '1--', '1-/1', '1', '1/1+', '1+', '1+/2-', '2-', '2-/2', '2', '2/2+', '2+', '2+/3-', '3-', '3-/3', '3', '3/3+',
+                             '3+', '3+/4-', '4-', '4-/4', '4', '4/4+', '4+', '4++']
+                g.DrawRectangle((col * BOX_SIZE + 5, row * BOX_SIZE + 3), (col * BOX_SIZE + BOX_SIZE + 5, row * BOX_SIZE + BOX_SIZE + 3), line_color='black',
+                                fill_color='#2196F3')
+                g.DrawText('{}'.format(arr_marks[col]), (col * BOX_SIZE + 13, row * BOX_SIZE + 10))
+
+            else:
+                v_table_data = cur.execute(
+                    """SELECT expectation, x_inc, x_r, x_1mm, x_1ms1, x_1, x_1s1p, x_1p, x_1ps2m, x_2m, x_2ms2, x_2, x_2s2p, x_2p, x_2ps3m, x_3m, x_3ms3, x_3, x_3s3p, x_3p, x_3ps4m, x_4m, x_4ms4, x_4, x_4s4p, x_4p, x_4pp FROM EOM_MAIN_SCREEN_LAYOUT WHERE STUDENT_ID = :student_id""",
+                    student_id=student_id)
+                raw_data = cur.fetchall()
+                cooked_data = [n[col] for n in raw_data]
+                cooked_data = ['' if v is None else v for v in cooked_data]
+
+                # print(raw_data)
+
+>>>>>>> parent of 66bbc36... class selection is basically done, strange problem with the linking to grades_chart
 =======
 sort_id = 1
 
