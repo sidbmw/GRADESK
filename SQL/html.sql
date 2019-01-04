78adc1e82e0f63525169b5f08c3f3bf3708a345d
Siddharth Natamai <siddharthnatamai@gmail.com>
2019-01-03 23:18:20 -0500

    DONE!!!!

--- SQL/html.sql
+++ SQL/html.sql
@@ -2,7 +2,10 @@ set markup html on spool on entmap off
 set feedback off
 
 spool C:\Users\siddh\Documents\ICS4U\Reports\report.htm
+
 select * from eom_students;
+
+select * from eom_marks;
 
 exit
 
